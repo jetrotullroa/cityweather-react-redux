6b43b550e940781b0d15ed48fa7d2cbfb5b37940
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-19 20:36:16 +0800

    create chart for temp pressure and humidity data

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {Sparklines, SparklinesLine} from 'react-sparklines'
 
 
 class WeatherList extends React.Component {
 
   renderWeather(cityData) {
+
+    const cityId = cityData.city.id
+    const cityName = cityData.city.name
+    const cityTemps = cityData.list.map(weather => weather.main.temp)
+    const cityPressures = cityData.list.map(weather => weather.main.pressure)
+    const cityHumidities = cityData.list.map(weather => weather.main.humidity)
+
+
     return (
-      <tr key={cityData.city.id}>
-        <td>{cityData.city.name}</td>
-        <td></td>
-        <td></td>
-        <td></td>
+      <tr key={cityId}>
+        <td>{cityName}</td>
+        <td>
+          <Sparklines height={120} width={180} data={cityTemps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines height={120} width={180} data={cityPressures}>
+            <SparklinesLine color="yellow" />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines height={120} width={180} data={cityHumidities}>
+            <SparklinesLine color="green" />
+          </Sparklines>
+        </td>
       </tr>
     )
   }
