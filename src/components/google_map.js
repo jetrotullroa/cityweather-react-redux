e33686c7afc877bd4d990eb451d1a30ff4d11585
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-20 11:00:09 +0800

    create component that will handle googlemaps

---
+++ src/components/google_map.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+class GoogleMap extends React.Component {
+
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
+    })
+  }
+
+
+  render() {
+    return(
+      <div ref="map"></div>
+    )
+  }
+}
+
+export default GoogleMap
