7054830f925c3bb9453e6ed3cf94c74d3f4727c3
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-20 11:01:40 +0800

    render map to weather table

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Chart from '../components/chart'
+import GoogleMap from '../components/google_map'
 
 class WeatherList extends React.Component {
 
@@ -11,11 +12,13 @@ class WeatherList extends React.Component {
     const cityTemps = cityData.list.map(weather => weather.main.temp)
     const cityPressures = cityData.list.map(weather => weather.main.pressure)
     const cityHumidities = cityData.list.map(weather => weather.main.humidity)
+    const cityLatitude = cityData.city.coord.lat
+    const cityLongitude = cityData.city.coord.lon
 
-
+    console.log(cityLongitude, cityLatitude)
     return (
       <tr key={cityId}>
-        <td className="col-md-3">{cityName}</td>
+        <td className="col-md-3"><GoogleMap lat={cityLatitude} lon={cityLongitude} /></td>
         <td className="col-md-3"><Chart color="red" data={cityTemps} unit="K" /></td>
         <td className="col-md-3"><Chart color="green" data={cityPressures} unit="hPa" /></td>
         <td className="col-md-3"><Chart color="blue" data={cityHumidities} unit="%" /></td>
